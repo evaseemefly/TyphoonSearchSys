7079dc1ab098c056732a4fc416befb9648c4bc88
seriousSam <stupidanimal@outlook.com>
2019-02-24 02:32:38 +0800

    添加一个菜单

--- webclient/src/main.ts
+++ webclient/src/main.ts
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import jquery from "jquery";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
