12521afebdee1806fb3fad58741b72cfd1153baa
casablanca-mac-2016 <evaseemefly@126.com>
2019-02-26 21:14:32 +0800

    添加一个菜单
    Co-Authored-By: sihan <evaseemefly@126.com>

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <LeftBar/>
+    <!-- <router-view/> -->
   </div>
 </template>
+<script>
+import LeftBar from "@/views/Main/LeftBar/LeftBar";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    LeftBar
+  }
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
