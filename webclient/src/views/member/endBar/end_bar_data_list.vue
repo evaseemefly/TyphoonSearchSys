159851cb6d0ff3f9ec265db0a4de38705fd97487
casablanca-mac-2016 <evaseemefly@126.com>
2019-02-27 21:33:55 +0800

    1-增加部分父组件向子组件传值

--- webclient/src/views/member/endBar/end_bar_data_list.vue
+++ webclient/src/views/member/endBar/end_bar_data_list.vue
@@ -1,20 +1,12 @@
 <template>
-  <div
-    id="end_bar_div"
-    class="col-md-6 card-group"
-  >
-    <div class="card bg-info">
-      <div class="card-header">指定台风名称</div>
+  <div id="end_bar_div" class="col-md-6 card-group">
+    <div class="card bg-info col-md-12">
+      <div class="card-header">台风编号{{target_typhoon.code}}</div>
       <div class="card-body">
         <div class="row">
           <div class="col">
             <ul class="list-group">
-              <li
-                class="list-group-item"
-                v-for="(item,index) in date_list"
-                :key="index"
-                @click="onClick(item)"
-              >
+              <li class="list-group-item-sm" v-for="(item,index) in date_list" :key="index" @click="onClick(item)">
                 {{item|formatDate}}
               </li>
             </ul>
@@ -22,7 +14,10 @@
         </div>
       </div>
     </div>
-    <endData></endData>
+    <transition name="fade">
+      <endData v-show="is_show"></endData>
+    </transition>
+
   </div>
 </template>
 
@@ -39,7 +34,8 @@ import endDataList from "../endBar/end_data_list.vue";
     }
   },
   props: {
-    target_typhoon: DataList_Mid_Model // 由二级菜单传入的选中的台风
+    target_typhoon: DataList_Mid_Model, // 由二级菜单传入的选中的台风
+    code: String
   }
 })
 // @Component({
@@ -57,9 +53,12 @@ export default class end_bar_data_list extends Vue {
     new Date(2019, 2, 19),
     new Date(2019, 2, 20)
   ]; // 加载的时间列表
-
+  is_show: boolean = false;
   loadDateList(target_typhoon: DataList_Mid_Model): void {
     // 1 后台请求加载当前的date_list
+  }
+  onClick(obj: DataList_Mid_Model): void {
+    this.is_show = true;
   }
 }
 </script>
@@ -68,6 +67,7 @@ export default class end_bar_data_list extends Vue {
 #end_bar_div {
   /* position: absolute; */
   display: flex;
+  flex-direction: column;
   top: 0px;
   /* margin-left: 460px; */
   /* background: rgb(166, 187, 187); */
--- webclient/src/views/member/secondBar/second_bar_condition_search.vue
+++ webclient/src/views/member/secondBar/second_bar_condition_search.vue
@@ -1,134 +1,63 @@
 <template>
-  <div
-    id="condition"
-    class="col-md-6"
-  >
+  <div id="condition" class="col-md-8">
     <div class="col-md-6 subitem_div">
       <!-- 次级菜单，顶部搜索区域 -->
       <div class="card bg-info">
         <div class="card-header">多条件搜索</div>
         <div class="card-body">
           <div class="form-group row">
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_host"
-            >死亡</label>
+            <label class="col-form-label col-form-label-sm" for="ds_host">死亡</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_host"
-                type="text"
-                placeholder="人数"
-              />
+              <input class="form-control form-control-sm" id="ds_host" type="text" placeholder="人数" />
             </div>
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_host"
-            >损失</label>
+            <label class="col-form-label col-form-label-sm" for="ds_host">损失</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="损失"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="损失" />
             </div>
 
           </div>
           <div class="form-group row">
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_username"
-            >增水</label>
+            <label class="col-form-label col-form-label-sm" for="ds_username">增水</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="增水"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="增水" />
             </div>
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_username"
-            >潮位</label>
+            <label class="col-form-label col-form-label-sm" for="ds_username">潮位</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="潮位"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="潮位" />
             </div>
           </div>
           <div class="form-group row">
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_username"
-            >级别</label>
+            <label class="col-form-label col-form-label-sm" for="ds_username">级别</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="级别"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="级别" />
             </div>
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_username"
-            >风速</label>
+            <label class="col-form-label col-form-label-sm" for="ds_username">风速</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="风速"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="风速" />
             </div>
           </div>
           <div class="form-group row">
-            <label
-              class="col-form-label col-form-label-sm"
-              for="ds_username"
-            >气压</label>
+            <label class="col-form-label col-form-label-sm" for="ds_username">气压</label>
             <div class="col-sm-4">
-              <input
-                class="form-control form-control-sm"
-                id="ds_username"
-                type="text"
-                placeholder="气压"
-              />
+              <input class="form-control form-control-sm" id="ds_username" type="text" placeholder="气压" />
             </div>
           </div>
           <div class="form-group row">
             <div class="col-sm-10">
-              <button
-                @click="is_show=!is_show"
-                class="btn btn-primary"
-              >搜索</button>
+              <button @click="is_show=!is_show" class="btn btn-primary">搜索</button>
             </div>
           </div>
         </div>
       </div>
       <!-- 次级菜单，搜索后加载的台风列表 -->
       <transition name="fade">
-        <div
-          class="card bg-info"
-          v-show="is_show"
-          id="ty_list"
-        >
+        <div class="card bg-info" v-show="is_show" id="ty_list">
           <div class="card-header">台风列表</div>
           <div class="card-body">
             <div class="row">
               <div class="col">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="(item,index) in data_list"
-                    :key="index"
-                    @click="onClick(item)"
-                  >
+                  <li class="list-group-item" v-for="(item,index) in data_list" :key="index" @click="onClick(item)">
                     {{item.name}}
                   </li>
                 </ul>
@@ -139,7 +68,10 @@
 
       </transition>
     </div>
-    <endBar></endBar>
+    <transition name="fade">
+      <endBar v-show="end_bar_show" :target_typhoon=typhoon :code=code></endBar>
+    </transition>
+
     <!-- <endDataList></endDataList> -->
     <!-- <endBarTest></endBarTest> -->
   </div>
@@ -165,10 +97,23 @@ export default class second_bar_condition_search extends Vue {
     new DataList_Mid_Model("台风3", 3, "code_c")
   ];
   is_show: boolean = false;
+  end_bar_show: boolean = false;
+  code: string = "";
+  typhoon: DataList_Mid_Model;
   // 组件方法也可以直接声明为实例的方法
-  onClick(message: string): void {
-    window.alert(message);
+  onClick(obj: DataList_Mid_Model): void {
+    var myself = this;
+    myself.code = obj.code;
+    myself.end_bar_show = true;
+    myself.typhoon = obj;
+    console.log(myself);
+    // window.alert(message.code);
+    // console.log(myself);
+    // console.log(message);
   }
+  // onClickEndBar():void{
+
+  // }
 }
 </script>
 
