d50dd8b9338ea10ca0f2366d2c6c5eb1ee8dde36
刘思晗-mac15 <evaseemefly@126.com>
2019-03-18 17:26:06 +0800

    1-实现对台风路径文件的录入mongodb 2-django需要实现查询mongodb的功能

--- webserver/TyphoonSystem/TyphoonSystem/settings.py
+++ webserver/TyphoonSystem/TyphoonSystem/settings.py
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+default_typhoon_code="(nameless)"
--- webserver/TyphoonSystem/apps/Typhoon/views.py
+++ webserver/TyphoonSystem/apps/Typhoon/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import APIView
 
+from TyphoonSystem import settings
 import json
 
 from .models import Point,GeoTyphoonRealData
@@ -22,7 +23,33 @@ class PointInfoView(APIView):
 
 class TyphoonRealDataView(APIView):
     def get(self,request):
-        realtime_list=GeoTyphoonRealData.objects.all()
+        code=request.GET.get("code",settings.default_typhoon_code)
+        # 根据台风的code获取该台风的气象数据
+        # code="MERBOK"
+        # realtime_list=GeoTyphoonRealData.objects.all()
+        realtime_list=GeoTyphoonRealData.objects.filter(code=code)
         json_data = GeoTyphoonRealDataSerializer(realtime_list, many=True).data
         # json_data=json.dump(realtime_list.data)
         return Response(json_data)
+
+class FilterByMonth(APIView):
+    '''
+        根据起始月份进行查询（或改成起止日期）
+    '''
+    def get(self,request):
+
+        pass
+
+class FilterByYear(APIView):
+    '''
+        根据年份进行查询
+    '''
+    def get(self,request):
+        pass
+
+class FilterByRange(APIView):
+    '''
+        根据
+    '''
+    def get(self,request):
+        pass
