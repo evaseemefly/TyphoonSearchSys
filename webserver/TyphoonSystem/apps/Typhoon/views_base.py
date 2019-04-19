423623a3c19dbe2229485247ff220b20b1a24a5f
evaseemefly <evaseemefly@126.com>
2019-04-19 17:26:19 +0800

    1-加入抽箱子类

--- webserver/TyphoonSystem/apps/Typhoon/views_base.py
+++ webserver/TyphoonSystem/apps/Typhoon/views_base.py
@@ -1,4 +1,6 @@
 from rest_framework.decorators import APIView
+import abc
+from datetime import datetime
 
 class BaseView(APIView):
     def getTyphoonList(self,*args,**kwargs):
@@ -9,3 +11,15 @@ class BaseView(APIView):
         :return:
         '''
         pass
+
+class BaseDetailListView(abc.ABC):
+    '''
+        抽象子类，部分方法由父类实现
+    '''
+
+    @abc.abstractmethod
+    def getlist(self,startdate:datetime,*args,**kwargs):
+        '''
+            根据传入的startdate获取符合条件的观测数据
+        :return:
+        '''
