2c2f7097a2eec7fdb3c81278af87510604347300
evaseemefly-aw <evaseemefly@126.com>
2019-04-29 15:15:47 +0800

    加入了读取word提取其中内容的部分

---
+++ background/2-wordOpt/project/core/main.py
@@ -0,0 +1,23 @@
+from docx import Document
+import os
+
+targetfilename=r"5622.docx"
+targetpath=r"D:\01proj\typhoon\TyphoonSearchSys\demo_data"
+fullname=os.path.join(targetpath,targetfilename)
+
+
+def readtxt(filename):
+    '''
+        读取word文档
+    :param filename:
+    :return:
+    '''
+    doc = Document(filename)
+    fullText = []
+    # ((lambda x: fullText.append(x) )(para))
+    for para in doc.paragraphs:
+        if len(para.text)>0:
+            fullText.append(para.text)
+    return '\n'.join(fullText)
+
+print (readtxt(fullname))
