bb4de3396a4d36b9fb46c86d1afa33e750f9b649
evaseemefly-aw <evaseemefly@126.com>
2019-05-04 22:40:47 +0800

    1-将复杂单页面vue组件拆分（vue-lodaer支持拆分html、js、css; 2-使用mixin的方式对vue中script中的data拆分成单独的文件。 ————仅修改了form_text子组件还需修改map_range

--- webclient/src/views/member/form/text_form.vue
+++ webclient/src/views/member/form/text_form.vue
@@ -1,19 +1,4 @@
-<template>
-  <form id="main">
-    <button type="button" @click="isActive=!isActive" class="btn btn-info ani">灾情信息</button>
-    <MyCollapse>
-      <div v-show="isActive" id="textform" class="card" style="width: 18rem;">
-        <div class="card-body" v-html="text">
-          <h5 class="card-title">{{title}}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">台风编号</h6>
-          <p class="card-text">{{text}}</p>
-          <!-- <p class="card-text">{{targetTyphoon}}</p> -->
-          <a href="#" class="card-link">当前台风</a>
-        </div>
-      </div>
-    </MyCollapse>
-  </form>
-</template>
+<template src="./text_form.html"></template>
 
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
@@ -33,13 +18,20 @@ import collapse from "@/common/effectControls/collapse.ts";
 // 后台请求的api
 import { ITyphoonRealDataParamas, loadTyphoonWord } from "@/api/api.ts";
 
+// TODO:[*] 19-05-04 将data从vue主文件中拆分
+// 使用mixin的方式，不使用拓展分隔符的方式
+import TextFormDataMixin from "./text_form_data_mixin";
+import { mixins } from "vue-class-component";
+
 @Component({
   components: { MyCollapse: collapse }
 })
-export default class text_form extends Vue {
-  title: string = "";
-  text: string = "";
-  isActive: boolean = false;
+// TODO:[*] 19-05-04 将data拆分至外部的ts文件，并使用mixin的方式载入，此处备份
+// export default class text_form extends Vue {
+export default class text_form extends mixins(TextFormDataMixin) {
+  // title: string = "";
+  // text: string = "";
+  // isActive: boolean = false;
 
   // 当前选择的台风(由vuex获取)
   get targetTyphoon(): MeteorologyRealData_Mid_Model {
@@ -74,73 +66,5 @@ export default class text_form extends Vue {
 }
 </script>
 
-<style scoped>
-#main {
-  position: fixed;
-  top: 100px;
-  right: 50px;
-  /* height: 200px; */
-  /* background: red; */
-  z-index: 9999;
-}
-#main button {
-  margin-bottom: 10px;
-}
-#textform {
-  background: linear-gradient(to right, #2c3e50 30%, rgba(4, 107, 114, 0.639));
-  max-height: 600px;
-  overflow-y: scroll;
-}
-/* 设置自定义的滚动条效果： */
-#textform::-webkit-scrollbar-track {
-  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  background-color: #2c3e50;
-  border-radius: 10px;
-}
-
-#textform::-webkit-scrollbar {
-  width: 10px;
-  background-color: #2c3e50;
-}
-
-/* 这么设置滚动条的透明度无效 */
-/* #textform::-webkit-scrollbar-track {
-  opacity: 0.4;
-} */
-
-/* #textform::-webkit-scrollbar-track {
-  opacity: 0.4;
-} */
-
-#textform::-webkit-scrollbar-thumb {
-  border-radius: 10px;
-  background-image: -webkit-gradient(
-    linear,
-    left bottom,
-    left top,
-    color-stop(0.44, rgb(122, 153, 217)),
-    color-stop(0.72, rgb(73, 125, 189)),
-    color-stop(0.86, rgb(28, 58, 148))
-  );
-}
-
-/* btn加入呼吸效果  */
-.ani {
-  -webkit-animation: blink 2s infinite ease-in-out;
-}
-
-@-webkit-keyframes blink {
-  0% {
-    box-shadow: 0px 0px 10px rgb(243, 196, 40);
-  }
-
-  50% {
-    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-    border-width: 0;
-  }
-
-  100% {
-    box-shadow: 0px 0px 10px rgb(243, 196, 40);
-  }
-}
+<style src="./text_form.css" scoped>
 </style>
