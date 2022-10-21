fbf61377fff2341ec1116a27befc8368448284ef
sihan <evaseemefly@126.com>
2022-10-21 10:49:01 +0800

    title:22-10-21 原webclient api

--- webclient/src/api/api.ts
+++ webclient/src/api/api.ts
@@ -4,7 +4,8 @@ import { MenuType } from '@/common/enum/menu.ts'
 // 后端的请求地址及端口
 // export const host = 'http://127.0.0.1:8000'
 // 实际部署地址及端口
-export const host = 'http://128.5.10.26:8000'
+// export const host = 'http://128.5.10.26:8000'
+export const host = 'http://192.168.0.105:8000'
 // 实际部署地址及端口
 // export const host = 'http://128.5.10.26:8000'
 // 本地docker环境
