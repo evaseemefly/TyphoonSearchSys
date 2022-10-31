5e1d588ce1f8165d1d6a8533d00fd46bd1d33126
sihan <evaseemefly@126.com>
2022-10-31 16:37:25 +0800

    title:22-10-31 加入了 thumbs/thumbListView.vue 与 thumbs/baseThumbView.vue 实现了缩略按钮

---
+++ webclient_new/ty_search_client/src/components/thumbs/baseThumbView.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="thumb-btn">
+		<span>{{ title }}</span>
+		<div class="thumb-btn fa-solid fa-up-right-and-down-left-from-center"></div>
+		<div class="thumb-btn fa-solid fa-minus"></div>
+	</div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+/** 基础的缩略按钮视图 */
+@Component({})
+export default class BaseThumbView extends Vue {
+	// title = '测试标题'
+	@Prop({ default: '默认标题' })
+	title: string
+}
+</script>
+<style scoped lang="less">
+@import url('../../styles/base-form.less');
+.thumb-btn {
+	display: flex;
+	align-items: center;
+	padding-left: 10px;
+	padding-right: 10px;
+	padding-top: 5px;
+	padding-bottom: 5px;
+	// background: black;
+	@form-base-shadow();
+	@form-base-radius();
+	margin: 10px;
+	color: white;
+
+	.thumb-btn {
+		padding: 5px;
+		margin-left: 5px;
+		margin-right: 5px;
+	}
+}
+</style>
---
+++ webclient_new/ty_search_client/src/components/thumbs/thumbListView.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="thumb-list">
+		<BaseThumbView title="台风查询列表"></BaseThumbView>
+		<BaseThumbView title="站点极值列表"></BaseThumbView>
+		<BaseThumbView title="站点详情"></BaseThumbView>
+	</div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+
+import BaseThumbView from '@/components/thumbs/baseThumbView.vue'
+@Component({ components: { BaseThumbView } })
+export default class ThumbListView extends Vue {
+	mydata: any = null
+	mounted() {}
+	get computedTest() {
+		return null
+	}
+}
+</script>
+<style scoped lang="less">
+.thumb-list {
+	display: flex;
+	position: absolute;
+	top: 50px;
+	left: 100px;
+	z-index: 9999;
+}
+</style>
