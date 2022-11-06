4d9ca75efd3eaa1a746156c83ebc39cc896a962c
evaseemefly <evaseemefly@126.com>
2022-11-06 11:24:19 +0800

    title：22-11-06 5750 修改，引发了错误!!

--- webclient_new/ty_search_client/.prettierrc.js
+++ webclient_new/ty_search_client/.prettierrc.js
@@ -4,6 +4,7 @@ module.exports={
     printWidth: 100, // 一行的字符数，如果超过会进行换行，默认为80
 	tabWidth: 4, // 一个tab代表几个空格数
 	useTabs: true, // 启用tab缩进
+    endOfLine:auto
     // 对象的 key 仅在必要时用引号
     // quoteProps: 'as-needed',
 }
